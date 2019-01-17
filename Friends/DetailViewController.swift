7fa4f203e78aa6607f3a8e5e228a1846b8a0444c
David Doswell <davidoliverdoswell@icloud.com>
2019-01-17 12:16:53 -0800

    Initial Commit

---
+++ Friends/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  Friends
+//
+//  Created by David Doswell on 1/17/19.
+//  Copyright © 2019 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bioLabel: UILabel!
+}
---
+++ Friends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by David Doswell on 1/17/19.
+//  Copyright © 2019 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
